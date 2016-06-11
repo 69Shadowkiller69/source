3cf3af49d81571bed1ddf5af17c12bcc894eec1d
69Shadowkiller69 <santerivr@hotmail.com>
2016-06-11 12:07:47 +0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -99,7 +99,7 @@
     var loadChat = function (cb) {
         if (!cb) cb = function () {
         };
-        $.get("https://raw.githubusercontent.com/basicBot/source/master/lang/langIndex.json", function (json) {
+        $.get("https://github.com/69Shadowkiller69/source/blob/master/lang/langIndex.json", function (json) {
             var link = basicBot.chatLink;
             if (json !== null && typeof json !== "undefined") {
                 langIndex = json;
@@ -2476,7 +2476,7 @@
                         if (msg.length <= cmd.length + 1) return API.sendChat(subChat(basicBot.chat.currentlang, {language: basicBot.settings.language}));
                         var argument = msg.substring(cmd.length + 1);
 
-                        $.get("https://rawgit.com/basicBot/source/master/lang/langIndex.json", function (json) {
+                        $.get("https://raw.githubusercontent.com/69Shadowkiller69/source/master/lang/langIndex.json", function (json) {
                             var langIndex = json;
                             var link = langIndex[argument.toLowerCase()];
                             if (typeof link === "undefined") {
